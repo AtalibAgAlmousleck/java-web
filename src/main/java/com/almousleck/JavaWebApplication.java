dc86bfbedd8196ec7f6709f16f99fceac2b06ea6
AtalibAgAlmousleck <atalibletouareg@gmail.com>
2024-03-13 11:41:41 +0800

    Github action

--- src/main/java/com/almousleck/JavaWebApplication.java
+++ src/main/java/com/almousleck/JavaWebApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaWebApplication {
     public static void main(String[] args) {
+        //-Dmaven.tests-skip=true
         SpringApplication.run(JavaWebApplication.class, args);
     }
 }
